6c33867f38605ed831aaf3fd271f06c6041b95da
D Geula <gonkhead112@gmail.com>
2022-09-26 19:56:52 +0100

    week 2

--- clean_json.py
+++ clean_json.py
@@ -1,9 +1,12 @@
 import json
 
 
-with open('data.json', encoding='utf-8') as json_file:
+# with open('data.json', encoding='utf-8') as json_file:
+#     data = json.load(json_file)
+
+with open('clean_data.json', encoding='utf-8') as json_file:
     data = json.load(json_file)
 
 # dump the edited json data to a file
-with open('test_data.json', 'w', encoding='utf-8') as outfile:
+with open('clean_data.json', 'w', encoding='utf-8') as outfile:
     json.dump(data, outfile, ensure_ascii=False, indent=4)
